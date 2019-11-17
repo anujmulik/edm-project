cfd1c762438dfeefa5d91cd29b55f6975d214391
Anuj Mulik <anuj9306@gmail.com>
2019-11-17 10:51:26 -0700

    Adding table to GUI

--- src/main/zipcar-ui/src/App.js
+++ src/main/zipcar-ui/src/App.js
@@ -1,36 +1,69 @@
 import React from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
-class App extends React.Component
-{
+const classes = {
+    root: {
+        width: '100%',
+        overflowX: 'auto',
+    },
+    table: {
+        minWidth: 650,
+    },
+};
+
+
+class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            employees: []
+
+        }
+    }
+
   componentDidMount() {
-    axios.get(`/api/greeting`)
+    axios.get(`/api/employees/all`)
         .then(res => {
-          console.log(res.data);
+          this.setState({
+              employees: res.data
+          }, () => console.log(this.state.employees));
         })
   }
 
   render()
   {
     return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo"/>
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
+        <Paper className={classes.root}>
+            <Table className={classes.table} aria-label="simple table">
+                <TableHead>
+                    <TableRow>
+                        <TableCell>Employee ID</TableCell>
+                        <TableCell align="right">First Name</TableCell>
+                        <TableCell align="right">Last Name</TableCell>
+                        <TableCell align="right">Type</TableCell>
+                    </TableRow>
+                </TableHead>
+                <TableBody>
+                    {this.state.employees.map(row => (
+                        <TableRow key={row.EMPLOYEE_ID}>
+                            <TableCell component="th" scope="row">
+                                {row.EMPLOYEE_ID}
+                            </TableCell>
+                            <TableCell align="right">{row.FIRST_NAME}</TableCell>
+                            <TableCell align="right">{row.LAST_NAME}</TableCell>
+                            <TableCell align="right">{row.TYPE}</TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
+        </Paper>
     );
   }
 }
